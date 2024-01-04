a1628ef78cc665132305bc02ca411b96085025e0
Bojan <32612389+bojanche@users.noreply.github.com>
2024-01-04 11:40:09 +0100

    iz firme

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,32 +1,42 @@
 
 #include <Preferences.h>
 #include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
-
+#define WIFIRSTPIN 23
 
 Preferences preferences;
+AsyncWebServer server(80);
 
-const char* ssid     = "Object";
-const char* password = "1234567890";
+String wifiMacString = WiFi.macAddress();
+String appendToSsid = wifiMacString.substring(9,11)+wifiMacString.substring(12,14)+wifiMacString.substring(15,17);
+String ssid     = "Object_"+appendToSsid;
+String password = "1234567890";
 String ssid_sta;
 String pass_sta;
 
 void setup()
 {
     Serial.begin(115200);
+    pinMode(WIFIRSTPIN, INPUT_PULLUP);
     preferences.begin("wifi", false);
     ssid_sta=preferences.getString("ssid");
     pass_sta=preferences.getString("pass");
     if (ssid_sta == ""){
-    Serial.println("\n[*] Creating AP");
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(ssid, password);
-    Serial.print("[+] AP Created with IP Gateway ");
-    Serial.println(WiFi.softAPIP());
+        Serial.println("\n[*] Creating AP");
+        WiFi.mode(WIFI_AP);
+        WiFi.softAP(ssid, password);
+        Serial.print("[+] AP Created with IP Gateway ");
+        Serial.println(WiFi.softAPIP());
     }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (digitalRead(WIFIRSTPIN)==0) {
+    preferences.clear();
+    preferences.end();
+    ESP.restart();
+  }
 }
